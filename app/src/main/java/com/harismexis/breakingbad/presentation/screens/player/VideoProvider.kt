4b9b6a30e98efef9e4c7275905de1e567fbbd99a
Haris Mexis <theoharismexis@hotmail.com>
2021-04-15 19:42:40 +0100

    updates

--- app/src/main/java/com/harismexis/breakingbad/presentation/screens/player/VideoProvider.kt
+++ app/src/main/java/com/harismexis/breakingbad/presentation/screens/player/VideoProvider.kt
@@ -10,14 +10,50 @@ fun provideVideos(): ArrayList<VideoItem> {
     )
     videos.add(
         VideoItem(
+            "zzyudpED6sg",
+            "Every death in Breaking Bad"
+        )
+    )
+    videos.add(
+        VideoItem(
+            "Xs6_vecSv2Y",
+            "Breaking Bad Greatest Moments"
+        )
+    )
+    videos.add(
+        VideoItem(
+            "c9BW9dCVYzA",
+            "Hank and Walt"
+        )
+    )
+    videos.add(
+        VideoItem(
             "pgxFTghg0lI",
             "Breaking Bad Season 3: Episode 7: Killing the Twins HD CLIP"
         )
     )
     videos.add(
         VideoItem(
-            "zzyudpED6sg",
-            "Every death in Breaking Bad"
+            "6P7XzoTJRa8",
+            "Breaking Bad Season 5: Episode 14: The phone call HD CLIP"
+        )
+    )
+    videos.add(
+        VideoItem(
+            "QLUG7jSUR08",
+            "Breaking Bad Season 5: Episode 7: Walt kills Mike HD CLIP"
+        )
+    )
+    videos.add(
+        VideoItem(
+            "5wOvXusjqgI",
+            "Breaking Bad Season 5: Episode 9: The confrontation HD CLIP"
+        )
+    )
+    videos.add(
+        VideoItem(
+            "8-aOkHmo-_c",
+            "Breaking Bad Season 5: Episode 10: That money thing HD CLIP"
         )
     )
     return videos
