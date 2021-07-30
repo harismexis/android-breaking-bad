b01b1c62b1fa61aad8379face605153ecf3d5c04
Haris Mexis <theoharismexis@hotmail.com>
2021-07-30 15:48:26 +0100

    updates

--- app/src/main/java/com/harismexis/breakingbad/framework/di/BreakingBadApiModule.kt
+++ app/src/main/java/com/harismexis/breakingbad/framework/di/BreakingBadApiModule.kt
@@ -9,16 +9,19 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class BreakingBadApiModule {
 
     @Provides
+    @Singleton
     fun provideBreakingBadApi(retrofit: Retrofit): BreakingBadApi {
         return retrofit.create(BreakingBadApi::class.java)
     }
 
     @Provides
+    @Singleton
     fun provideRetrofit(
         okHttpClient: OkHttpClient,
         gson: Gson
@@ -31,11 +34,13 @@ class BreakingBadApiModule {
     }
 
     @Provides
+    @Singleton
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder().build()
     }
 
     @Provides
+    @Singleton
     fun provideGSON(): Gson {
         return GsonBuilder().setLenient().create()
     }
